5429a40ba495133621fb8447e91f2c0d07f87321
HarrisLeow <leow_harris@hotmail.sg>
2017-04-02 21:48:39 -0700

    Added screens with redux scenes

---
+++ src/reducers/EmployeeFormReducer.js
@@ -0,0 +1,20 @@
+import {
+  EMPLOYEE_UPDATE
+} from '../actions/types'
+
+const INITIAL_STATE = {
+  name: '',
+  phone: '',
+  shoft: ''
+}
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case EMPLOYEE_UPDATE:
+      // action.payload === { prop: 'name', value: 'jane' }
+      // Use key interpolation
+      return { ...state, [action.payload.prop]: action.payload.value }
+    default:
+      return state
+  }
+}
