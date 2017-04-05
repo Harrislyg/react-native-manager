19d428c3e3a493228321f98ae5b82cb85083723a
HarrisLeow <leow_harris@hotmail.sg>
2017-04-05 00:55:33 -0700

    create users

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,5 +1,8 @@
+import firebase from 'firebase'
+import { Actions } from 'react-native-router-flux'
 import {
-  EMPLOYEE_UPDATE
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE
 } from './types'
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -11,5 +14,16 @@ export const employeeUpdate = ({ prop, value }) => {
 }
 
 export const employeeCreate = ({ name, phone, shift }) => {
-  console.log(name, phone, shift)
+  // Get the current user object that we have created
+  const { currentUser } = firebase.auth()
+  // Extract the uid from the user object
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .push({ name, phone, shift })
+      // type: reset will remove the back button
+      .then(() => {
+        dispatch({ type: EMPLOYEE_CREATE })
+        Actions.employeeList({ type: 'reset' })
+      })
+  }
 }
