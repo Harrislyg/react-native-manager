bbffeede1f715dffafc12a96dc82e40685ea3c6b
HarrisLeow <leow_harris@hotmail.sg>
2017-04-09 00:34:37 -0700

    separate component for editing user

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import { Picker, Text } from 'react-native'
-//import { View, Text } from 'react-native'
+// import { Picker, Text } from 'react-native'
+// import { View, Text } from 'react-native'
 import { connect } from 'react-redux'
 import { employeeUpdate, employeeCreate } from '../actions'
-import { Card, CardSection, Input, Button } from './common'
+import { Card, CardSection, Button } from './common'
+import EmployeeForm from './EmployeeForm'
 
 class EmployeeCreate extends Component {
   // Since CardSection is a component that I created, the style prop is not automatically interpreted. I need to manually add that prop in the CardSection component file
@@ -12,45 +13,10 @@ class EmployeeCreate extends Component {
 
     this.props.employeeCreate({ name, phone, shift: shift || 'Monday' })
   }
-
   render () {
     return (
       <Card>
-        <CardSection>
-          <Input
-            label='Name'
-            placeholder='Jane'
-            value={this.props.name}
-            onChangeText={value => this.props.employeeUpdate({prop: 'name', value})}
-          />
-        </CardSection>
-
-        <CardSection>
-          <Input
-            label='Phone'
-            placeholder= '555-555-555'
-            value={this.props.phone}
-            onChangeText={value => this.props.employeeUpdate({prop: 'phone', value})}
-          />
-        </CardSection>
-
-        <CardSection style={{ flexDirection: 'column' }}>
-          <Text style={styles.pickerTextStyle}>Shift</Text>
-          <Picker
-            style={{ flex: 1 }}
-            selectedValue={this.props.shift}
-            onValueChange={value => this.props.employeeUpdate({prop: 'shift', value})}
-          >
-            <Picker.Item label="Monday" value="Monday" />
-            <Picker.Item label="Tuesday" value="Tuesday" />
-            <Picker.Item label="Wednesday" value="Wednesday" />
-            <Picker.Item label="Thursday" value="Thursday" />
-            <Picker.Item label="Friday" value="Friday" />
-            <Picker.Item label="Saturday" value="Saturday" />
-            <Picker.Item label="Sunday" value="Sunday" />
-          </Picker>
-        </CardSection>
-
+        <EmployeeForm {...this.props} />
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
             Create
@@ -58,13 +24,6 @@ class EmployeeCreate extends Component {
         </CardSection>
       </Card>
     )
-  }
-}
-
-const styles = {
-  pickerTextStyle: {
-    fontSize: 18,
-    paddingLeft: 20
   }
 }
 
