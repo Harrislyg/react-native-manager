dd10928ebefaf82fbde4b4c10ac3aa68275c9bb1
HarrisLeow <leow_harris@hotmail.sg>
2017-04-15 02:12:26 -0700

    Modal added to delete from Firebase

--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -6,6 +6,7 @@ import { Button } from './Button'
 
 
 const Confirm = ({ children, visible, onAccept, onDecline }) => {
+  const { containerStyle, textStlye, cardSectionStyle } = styles
   return (
       <Modal
         visible={visible}
@@ -13,9 +14,11 @@ const Confirm = ({ children, visible, onAccept, onDecline }) => {
         animationType='slide'
         onRequestClose={() => {}}
       >
-        <View>
-          <CardSection>
-            <Text>{children}</Text>
+        <View style={containerStyle}>
+          <CardSection style={cardSectionStyle}>
+            <Text style={textStlye}>
+              {children}
+            </Text>
           </CardSection>
           <CardSection>
             <Button onPress={onAccept}>Yes</Button>
@@ -24,6 +27,24 @@ const Confirm = ({ children, visible, onAccept, onDecline }) => {
         </View>
       </Modal>
   )
+}
+
+const styles = {
+  cardSectionStyle: {
+    justifyContent: 'center'
+  },
+  textStlye: {
+    flex: 1,
+    fontSize: 18,
+    textAlign: 'center',
+    lineHeight: 40
+  },
+  containerStyle: {
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    position: 'relative',
+    flex: 1,
+    justifyContent: 'center'
+  }
 }
 
 export { Confirm }
